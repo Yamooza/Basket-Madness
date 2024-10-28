2f7500d98375d00a6352b35dd8f3daf6aaa60925
Yamooza <einovalta@gmail.com>
2024-10-28 09:49:48 +0200

    s

--- Assets/Scripts/DeathCol.cs
+++ Assets/Scripts/DeathCol.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathCol : MonoBehaviour
+public class DeathCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            Health playerHealth = other.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(playerHealth.maxHealth); // Deal enough damage to "kill" the player
+            }
+        }
     }
 }
