740ae60dd2245d7811d123942526f2aaba9f223c
Velho420420 <152969429+Velho420420@users.noreply.github.com>
2024-10-28 11:57:29 +0200

    s
    musa, soundit, ampuminen, mappi vähä muutettu

---
+++ Assets/Scripts/SoundEffectsPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectsPlayer : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip sfx1,sfx2,sfx3,sfx4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.W))
+    {
+        src.clip = sfx1;
+        src.Play();
+    }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        src.clip = sfx2;
+        src.Play();
+    }
+    }
+}
