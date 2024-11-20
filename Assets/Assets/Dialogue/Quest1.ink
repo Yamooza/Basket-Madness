00e1f28f902a4bec518de3e058cc500202efd148
Yamooza <einovalta@gmail.com>
2024-11-20 10:30:00 +0200

    Dialogue multiple choice done

---
+++ Assets/Assets/Dialogue/Quest1.ink
@@ -0,0 +1,12 @@
+﻿-> main
+
+=== main ===
+Can you help me with something?
+    + [Yes]
+        -> chosen("Yes")
+    + [NO!]
+        -> chosen("NO!")
+
+=== chosen(question) ===
+You chose {question}!
+-> END
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private GameObject dialoguepanel;
 
     [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+
+    private TextMeshProUGUI[] choicesText;
+
 
     private Story currentStory;
 
@@ -35,6 +42,15 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguepanel.SetActive(false);
+
+        //get all choices texts
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
@@ -72,11 +88,53 @@ public class DialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+            //set text for the current dialogue line
             dialogueText.text = currentStory.Continue();
+            //displaye choices, if any, for this dialogue line
+            DisplayChoices();
         }
         else
         {
             StartCoroutine(ExitDialogueMode());
         }
     }
-}
+        private void DisplayChoices()
+        {
+            //defensive chect to make sure our UI can support the number of choices
+            List<Choice> currentChoices = currentStory.currentChoices;
+
+            if (currentChoices.Count > choices.Length)
+            {
+                Debug.LogError("More choices given than the UI can support. Number of choices given: " 
+                    + currentChoices.Count);
+            }
+            int index = 0;
+            //enable and initialize the choices up to the amount of choices for this line of dialogue
+            foreach (Choice choice in currentChoices)
+            {
+                choices[index].gameObject.SetActive(true);
+                choicesText[index].text = choice.text;
+                index++;
+            }
+            //go through the remaining choices the UI supports and make sure they are hidden
+            for (int i = index; i < choices.Length; i++)
+            {
+                choices[i].gameObject.SetActive(false);
+            }
+            StartCoroutine(SelectFirstChoice());
+        }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        //event system requires we clear it first then wait
+        //for at least one frame before we set the current selected object.
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+    }
+
+    }
